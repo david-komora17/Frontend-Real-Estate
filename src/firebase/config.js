6713b9c8e5744693b3941cee82c6e205e4d0e12b
Gracesvg <134380789+Gracesvg@users.noreply.github.com>
2026-04-15 01:44:46 +0300

    Set up a firebase folder with an initialization script called config.js for your firebase project and extract the sensitive keys and place them in the environment variable file.

---
+++ src/firebase/config.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: VITE_FIREBASE_API_KEY,
+  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: "frontend-real-estate",
+  storageBucket: "frontend-real-estate.firebasestorage.app",
+  messagingSenderId: "792976618668",
+  appId: "1:792976618668:web:0ec91354a5b7eb647b5b09",
+  measurementId: "G-CD1Y57NQ9N"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
